110a1d416c78a09996f84a9f98aa3772b3dbe5db
rounakdatta <rounakdatta12@gmail.com>
2019-01-16 01:20:50 +0530

    async thread for GET
    re-write in async functions

--- app/src/main/java/tk/rounakdatta/messitup/MainActivity.java
+++ app/src/main/java/tk/rounakdatta/messitup/MainActivity.java
@@ -18,10 +18,12 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+
 
 public class MainActivity extends AppCompatActivity {
     Button btn;
@@ -31,94 +33,77 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn=findViewById(R.id.btnSubmit);
-        txt=findViewById(R.id.txtDisplay);
+        btn = findViewById(R.id.btnSubmit);
+        txt = findViewById(R.id.txtDisplay);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(getApplicationContext(),SecondActivity.class);
+
+//                // run the network connection activity in a new thread
+//                new Thread(new Runnable() {
+//
+//                    @Override
+//                    public void run() {
+//
+//                        try {
+//                            outputOfGETRequest = getFromURL("https://www.google.com");
+//                            System.out.println(outputOfGETRequest.toString());
+//                        } catch (Exception e) {
+//                            System.out.println("noooooooooooooo");
+//                        }
+//
+//                    }
+//
+//                }).start();
+
+                String homepageURL = "https://www.google.com";
+                String homePage = "Hello Google";
+
+                HttpGetRequest foo = new HttpGetRequest();
+                foo.execute(homepageURL);
+
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
+
+//                // run the network connection activity in a new thread
+//                new Thread(new Runnable() {
+//
+//                    // initiate the okHttpClient
+//                    OkHttpClient client = new OkHttpClient();
+//
+//                    // function to get data from URL
+//                    String getFromURL(String url) throws IOException {
+//                        Request request = new Request.Builder()
+//                                .url(url)
+//                                .build();
+//
+//                        try (Response response = client.newCall(request).execute()) {
+//                            return response.body().string();
+//                        } catch (Exception e) {
+//                            System.out.println(e);
+//                            return "Error connecting to MessItUp network";
+//                        }
+//                    }
+//
+//                    String outputOfGETRequest;
+//
+//                    @Override
+//                    public void run() {
+//
+//                        try {
+//                            outputOfGETRequest = getFromURL("https://www.google.com");
+//                            System.out.println(outputOfGETRequest.toString());
+//                        } catch (Exception e) {
+//                            System.out.println("noooooooooooooo");
+//                        }
+//
+//                    }
+//
+//                }).start();
 
 
-                    new Thread(new Runnable(){
-
-                        OkHttpClient client = new OkHttpClient();
-
-                        String runn(String url) throws IOException {
-                            Request request = new Request.Builder()
-                                    .url(url)
-                                    .build();
-
-                            try (Response response = client.newCall(request).execute()) {
-                                return response.body().string();
-                            } catch(Exception e) {
-                                System.out.println(e);
-                                return "no";
-                            }
-                        }
-
-
-
-                        @Override
-                        public void run() {
-                            // Do network action in this function
-
-                            try {
-                                String foo = runn("https://www.google.com");
-                                System.out.println(foo.toString());
-                            } catch(Exception e) {
-                                System.out.println("noooooooooooooo");
-                            }
-
-                        }
-                    }).start();
-
-
-                intent.putExtra("Text","helllllllllllllo0o");
-                startActivity(intent);
-                txt.setText("Button presses!");
 
             }
         });
-
-//        URL url = new URL("http://www.android.com/");
-//        HttpURLConnection connection=new HttpURLConnection("http://google.com") {
-//            @Override
-//            public void disconnect() {
-//
-//            }
-//
-//            @Override
-//            public boolean usingProxy() {
-//                return false;
-//            }
-//
-//            @Override
-//            public void connect() throws IOException {
-//
-//            }
-//        };
     }
-
-
-
-//    public String doGet(String urlString) {
-////        try {
-////            URL url = new URL(urlString);
-////
-////            HttpURLConnection foo = (HttpURLConnection)url.openConnection();
-////
-////            BufferedReader bro = new BufferedReader(new InputStreamReader(foo.getInputStream()));
-////            System.out.println("RoD"+String.valueOf(bro));
-////            return "helloworld";
-////
-////        } catch(Exception e) {
-////            System.out.println(e);
-////            return "error!";
-////        }
-//
-//
-//
-//
-//    }
 
 }
